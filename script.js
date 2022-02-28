c27dda32092dd18e0d46d98c069005d6354d0faf
Ramin <raminshakooei@gmail.com>
2022-02-28 13:32:39 +0330

    checking database for validation deleted

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let wordset = [...wordlist];
 let word = wordset[Math.floor(Math.random() * wordset.length)];
 let wordch = word.split("");
 let winnertrue = false;
-let cellcombine = "";
 let activecell = 0;
 let rownum = 0;
 const key = document.querySelectorAll(".key:not(.del)");
@@ -21,78 +20,74 @@ const losts = document.querySelector(".losts");
 
 // checking the word is correct or not
 function checkword() {
-  if (wordlist.includes(cellcombine)) {
-    wordch = word.split("");
-    let cells = document.querySelectorAll(`${activerow[rownum]}>.cell`);
-    for (let ch = 0; ch < cells.length; ch++) {
-      if (wordch[ch] === cells[ch].textContent) {
-        cells[ch].classList.add("green");
-        wordch[ch] = "x";
-      }
+  wordch = word.split("");
+  let cells = document.querySelectorAll(`${activerow[rownum]}>.cell`);
+  for (let ch = 0; ch < cells.length; ch++) {
+    if (wordch[ch] === cells[ch].textContent) {
+      cells[ch].classList.add("green");
+      wordch[ch] = "x";
     }
-    for (let otherch = 0; otherch < cells.length; otherch++) {
-      if (wordch[otherch] === "x") {
-        continue;
-      } else if (cells[otherch].textContent === wordch[0]) {
-        cells[otherch].classList.add("yellow");
-        wordch[0] = "y";
-        continue;
-      } else if (cells[otherch].textContent === wordch[1]) {
-        cells[otherch].classList.add("yellow");
-        wordch[1] = "y";
-        continue;
-      } else if (cells[otherch].textContent === wordch[2]) {
-        cells[otherch].classList.add("yellow");
-        wordch[2] = "y";
-        continue;
-      } else if (cells[otherch].textContent === wordch[3]) {
-        cells[otherch].classList.add("yellow");
-        wordch[3] = "y";
-        continue;
-      } else if (cells[otherch].textContent === wordch[4]) {
-        cells[otherch].classList.add("yellow");
-        wordch[4] = "y";
-        continue;
-      } else {
-        cells[otherch].classList.add("gray");
-      }
-    }
-    if (
-      cells[0].classList.contains("green") &&
-      cells[1].classList.contains("green") &&
-      cells[2].classList.contains("green") &&
-      cells[3].classList.contains("green") &&
-      cells[4].classList.contains("green")
-    ) {
-      winnertrue = true;
-      setTimeout(won, 10);
+  }
+  for (let otherch = 0; otherch < cells.length; otherch++) {
+    if (wordch[otherch] === "x") {
+      continue;
+    } else if (cells[otherch].textContent === wordch[0]) {
+      cells[otherch].classList.add("yellow");
+      wordch[0] = "y";
+      continue;
+    } else if (cells[otherch].textContent === wordch[1]) {
+      cells[otherch].classList.add("yellow");
+      wordch[1] = "y";
+      continue;
+    } else if (cells[otherch].textContent === wordch[2]) {
+      cells[otherch].classList.add("yellow");
+      wordch[2] = "y";
+      continue;
+    } else if (cells[otherch].textContent === wordch[3]) {
+      cells[otherch].classList.add("yellow");
+      wordch[3] = "y";
+      continue;
+    } else if (cells[otherch].textContent === wordch[4]) {
+      cells[otherch].classList.add("yellow");
+      wordch[4] = "y";
+      continue;
     } else {
-      // changing current rows border to initial
-      let focusrow = document.querySelectorAll(`${activerow[rownum]}>.cell`);
-      focusrow.forEach((item) => {
-        item.style.borderColor = "initial";
-      });
-
-      // go to next row and change activecell to 0
-      rownum++;
-      activecell = 0;
-
-      // changing current rows border to initial
-      focusrow = document.querySelectorAll(`${activerow[rownum]}>.cell`);
-      focusrow.forEach((item) => {
-        item.style.borderColor = "#79b8e1";
-      });
-
-      // check if user loss the game
-      if (rownum === 6) {
-        status.textContent = `کلمه مورد نظر ${word} بود.`;
-        losts.textContent = Number(losts.textContent) + 1;
-        setCookie("lostsNum", Number(losts.textContent), 30);
-        winnertrue = true;
-      }
+      cells[otherch].classList.add("gray");
     }
+  }
+  if (
+    cells[0].classList.contains("green") &&
+    cells[1].classList.contains("green") &&
+    cells[2].classList.contains("green") &&
+    cells[3].classList.contains("green") &&
+    cells[4].classList.contains("green")
+  ) {
+    winnertrue = true;
+    setTimeout(won, 10);
   } else {
-    status.textContent = `این کلمه در دیتابیس موجود نمی‌باشد.`;
+    // changing current rows border to initial
+    let focusrow = document.querySelectorAll(`${activerow[rownum]}>.cell`);
+    focusrow.forEach((item) => {
+      item.style.borderColor = "initial";
+    });
+
+    // go to next row and change activecell to 0
+    rownum++;
+    activecell = 0;
+
+    // changing current rows border to initial
+    focusrow = document.querySelectorAll(`${activerow[rownum]}>.cell`);
+    focusrow.forEach((item) => {
+      item.style.borderColor = "#79b8e1";
+    });
+
+    // check if user loss the game
+    if (rownum === 6) {
+      status.textContent = `کلمه مورد نظر ${word} بود.`;
+      losts.textContent = Number(losts.textContent) + 1;
+      setCookie("lostsNum", Number(losts.textContent), 30);
+      winnertrue = true;
+    }
   }
 }
 // winning fucntion
@@ -184,7 +179,7 @@ newgame.addEventListener("click", function () {
     winnertrue = false;
     activecell = 0;
     rownum = 0;
-    cellcombine = "";
+
     let focusrow = document.querySelectorAll(`${activerow[rownum]}>.cell`);
     focusrow.forEach((item) => {
       item.style.borderColor = "#79b8e1";
@@ -221,12 +216,6 @@ key.forEach((item) => {
         if (activecell < 5 && cell[activecell].textContent === "") {
           cell[activecell].textContent = item.textContent;
           activecell++;
-          cellcombine =
-            cell[0].textContent +
-            cell[1].textContent +
-            cell[2].textContent +
-            cell[3].textContent +
-            cell[4].textContent;
         }
       }
       status.textContent = "";
